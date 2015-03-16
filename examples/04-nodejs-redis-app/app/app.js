5d116323991b38fa1894b89e78e90118bbb0d72b
John Alexander Bye <john@bye.fm>
2015-03-17 00:39:22 +0100

    Install node modules for the 04-project

--- examples/04-nodejs-redis-app/app/app.js
+++ examples/04-nodejs-redis-app/app/app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var redis = require('redis');
-var rediscli = redis.createClient(process.env.REDIS_PORT || 6379,
-                                  process.env.REDIS_HOST || '127.0.0.1');
+var rediscli = redis.createClient(process.env.REDIS_PORT_6379_TCP_PORT || 6379,
+                                  process.env.REDIS_PORT_6379_TCP_ADDR || '127.0.0.1');
 
 var app = express();
 app.get('/', function (req, res) {
+  console.log("** Handling incoming request");
   rediscli.incr('counter', function (err, count) {
     res.json({
       'count': count
@@ -13,3 +14,4 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000);
+console.log("** Server started listening...");
